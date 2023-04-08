4f1dadf3c06979a15be09f70540c3e993dbc45be
jordanthecs <dasean.best@gmail.com>
2023-04-07 23:59:59 -0400

    l

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let guessButton = document.querySelector('.user-button');
 let userGuess = document.querySelector('.user-text');
+let table = document.querySelector('.message');
 let row = 0;
 let randWord;
+let count = 1;
 
 function readTextFile(file){
     let rawFile = new XMLHttpRequest();
@@ -23,34 +25,52 @@ let wordToGuess = randWord[Math.floor(Math.random() * 5758)];
 console.log(wordToGuess);
 
 guessButton.addEventListener('click',function(){
+    count++;
     if(row >= 5){
-        console.log('You lost');
+        setTimeout(function(){
+            if(userGuess.value !== wordToGuess){
+                alert('YOU LOSE!');
+            }else{
+                alert('YOU WIN!');
+            }
+            userGuess.value = '';
+        },500);
     }else{
+        for(let i = 0;i<5;i++){
+            if(document.querySelector(`#m${row}-${i}`).style.backgroundColor !== 'green' || document.querySelector(`#m${row}-${i}`).style.backgroundColor !== 'yellow'){
+                document.querySelector(`#m${row}-${i}`).style.backgroundColor = 'grey';
+                document.querySelector(`#m${row}-${i}`).style.color = 'white';
+            }
+        }
         for(let i = 0;i<5;i++){
             document.querySelector(`#m${row}-${i}`).value = userGuess.value.split('')[i];
             if(document.querySelector(`#m${row}-${i}`).value === wordToGuess[i]){
                 document.querySelector(`#m${row}-${i}`).style.backgroundColor = 'green';
                 document.querySelector(`#m${row}-${i}`).style.color = 'white';
             }
-        }
-        for(let i = 0;i<5;i++){
+        }for(let i = 0;i<5;i++){
             for(let j = 0;j<5;j++){
-                if(document.querySelector(`#m${row}-${i}`).style.backgroundColor !== 'green'){
-                    if(document.querySelector(`#m${row}-${i}`).value === wordToGuess[j]){
-                        document.querySelector(`#m${row}-${i}`).style.backgroundColor = 'yellow';
+                    if(document.querySelector(`#m${row}-${i}`).style.backgroundColor !== 'green'){
+                        if(document.querySelector(`#m${row}-${i}`).value === wordToGuess[j] && document.querySelector(`#m${row}-${j}`).style.backgroundColor !== 'green'){
+                            document.querySelector(`#m${row}-${i}`).style.backgroundColor = 'yellow';
+                            document.querySelector(`#m${row}-${i}`).style.color = 'black';
+                        }
                     }
                 }
             }
-        }
-    }
-    setTimeout(function(){
-        if(userGuess.value !== wordToGuess){
-            console.log('L');
-        }else{
+        if(userGuess.value === wordToGuess){
+            setTimeout(function(){
             alert('YOU WIN!');
+            userGuess.value = '';
+        },500);
+        for(let i = 0;i<5;i++){
+            for(let j = 0;j<5;j++){
+                    document.querySelector(`#m${i}-${j}`).value = '';
+                }
+            }
         }
-        userGuess.value = '';
-    },500);
+    userGuess.value = '';
     row++;
+    }
     
 });
